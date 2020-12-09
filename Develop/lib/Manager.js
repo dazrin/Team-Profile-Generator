f9ad3c1b584bf1e1ae80c7190b2e2c09084bfc86
Dazrin Tioseco <xdazee@gmail.com>
2020-12-08 19:10:07 -0800

    created classes/ constructors for each role

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,17 @@
+const Employee = require("./Employee");
+
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+        this.role = "Manager";
+    }
+
+    getOfficeNumber() {
+        return this.officeNumber;
+    };
+
+};
+
+module.exports = Manager;
