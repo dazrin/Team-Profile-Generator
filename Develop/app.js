0ee74baceedab92be1294c817e1a01672eb00128
Dazrin Tioseco <xdazee@gmail.com>
2020-12-08 19:33:55 -0800

    added function to ask questions + pass the answer data to render function, function for asking specific questions based on the employee's role, a function to save the html to hdd

--- Develop/app.js
+++ Develop/app.js
@@ -5,13 +5,99 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 const questions = require ('./lib/questions');
-const cha
-
+const chalk = require('chalk'); //outputs color
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { type } = require("os");
+const team = []; // array to contain all employees
 
+// Function to ask questions to collect data + pass it to render function
+const askForTeamInfo = async () => {
+    //Ask what type of employee want to add to team
+    return await inquirer.prompt(questions.role)
+    .then(type => {
+        if(type.role !== 'generate'){ //if generate option selected then call render function
+            // if not then ask employee based questions
+            askEmployeeInfo(type);
+        } else {
+            // Ask if user wants to add Team Name
+            inquirer.prompt([
+                {
+                    type: 'confirm',
+                    name: 'yesNo',
+                    message: 'Would you like to add team Name? ',
+                },
+                {
+                    type: 'input',
+                    name: 'teamName',
+                    message: `What is your team's Name? `,
+                    when: (answer) => {
+                        return answers.yesNo;
+                    },
+                }
+            ])
+            .then(answer => {
+                console.log(chalk.yellow(`\nGenerating Team's Web page...`));
+                let teamName = '';
+                if (answer.teamName) {
+                    team = ' - ' + answer.teamName;
+                }
+            // Call render function to generate html page
+            const html = render(team, teamName);
+            saveFile(html); // save rendered html to file    
+            });
+        }
+    });
+}
+
+// Function to ask more questions based on employee type
+const askEmployeeInfo = async (type) => {
+    console.log(chalk.yellow('-').repeat(60)); // Print yellow divider line in console
+    return await inquirer.prompt(questions.employee)
+    .then(employeeInfo => {
+        // Ask additional questions based on role
+        inquirer.prompt(questions[type.role])
+        .then(roleAnswer => {
+            let allAnswers = {...employeeInfo, ...roleAnswer};
+            console.log('\n');
+            // Based on role, create an Employee object with it's respective class
+            // Push all employee objects into team array
+            const { name, id, email, officeNumber, github, school} = allAnswers;
+            switch (type.role) {
+                case 'manager':
+                    team.push(new Manager(name, id, email, officeNumber));
+                    askForTeamInfo();
+                    break;
+                case 'engineer':
+                    team.push(new Engineer(name, id, email, github));
+                    askForTeamInfo();
+                    break;
+                case 'intern':
+                    team.push(new Intern(name, id, email, school));
+                    askForTeamInfo();
+                    break;
+            }
+            return allAnswers;
+        })
+    })
+}
+
+// Function to save generated html to hdd
+const saveFile = (html) => {
+    // Check if output directory exists and create one if it doesn't
+    if(!fs.existsSync(OUTPUT_DIR)){
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFile(outputPath, html, (err) =>
+        err ? console.error(chalk.red(err)) : console.log(chalk.green('file successfully saved ') + chalk.cyan('team.html') + chalk.green(' to output directory :)'))
+    );
+}
+
+//Start app 
+console.log(chalk.cyan('\n=== Welcome to your ') + chalk.cyan.bold('Team Profile Summary ') + chalk.cyan('web page generator ===\n'));
+askForTeamInfo();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
